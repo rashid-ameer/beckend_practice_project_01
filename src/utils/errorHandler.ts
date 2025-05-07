a0d926747ba07254219a153e8c1c357496ed0edb
Rashid Ameer <r.ameer.offical@gmail.com>
2025-05-07 14:46:07 +0500

    Integrated error handling for ApiError in global errorHandler

--- src/utils/errorHandler.ts
+++ src/utils/errorHandler.ts
@@ -1,6 +1,7 @@
 import { ErrorRequestHandler } from "express";
 import HTTP_CODES from "../constants/httpCodes";
 import { ZodError } from "zod";
+import ApiError from "./apiError";
 
 const errorHandler: ErrorRequestHandler = async (error, req, res, _) => {
   console.log(`Path: ${req.path} ---- Method: ${req.method}`);
@@ -10,6 +11,13 @@ const errorHandler: ErrorRequestHandler = async (error, req, res, _) => {
     res
       .status(HTTP_CODES.BAD_REQUEST)
       .json({ message: error.errors[0].message });
+    return;
+  }
+
+  if (error instanceof ApiError) {
+    res
+      .status(error.statusCode)
+      .json({ message: error.message, errorCode: error.errorCode });
     return;
   }
 
